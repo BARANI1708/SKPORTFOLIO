3fcedbf6f1e9316613124fb0ac24551f61a7263e
BARANI1708 <147473737+BARANI1708@users.noreply.github.com>
2025-07-19 15:55:55 +0530

    Fix white screen issue: switch to HashRouter and add GitHub Pages SPA routing support

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import ThrillerBackground from "@/components/ThrillerBackground";
@@ -77,13 +77,13 @@ const App = () => {
           <CinematicTitleCard onFinish={() => setShowOverlay(false)} />
         )}
         <AnimatedMainContent show={!showOverlay}>
-          <BrowserRouter basename="/SKPORTFOLIO">
+          <HashRouter>
             <Routes>
               <Route path="/" element={<Index />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </AnimatedMainContent>
       </TooltipProvider>
     </QueryClientProvider>
